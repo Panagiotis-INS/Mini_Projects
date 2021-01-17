4ed5b05c9cee8d9ca1f79d81f449e25e3c42013d
Panagiotis_INS <39010461+Panagiotis-INS@users.noreply.github.com>
2021-01-17 22:10:54 +0200

    Add files via upload

---
+++ Random_password_generator/pass_gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##
+#seed=`date |cut -d " " -f 5`;
+pass="";
+seed=`date |cut -d " " -f 5`;
+echo $seed >.config;
+pass=`md5sum .config|cut -d " " -f 1 |base64 |head -c 10`;
+echo $pass;
+exit 0;
