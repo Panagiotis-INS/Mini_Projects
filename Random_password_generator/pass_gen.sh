fd8b5f579fcdfa6328615b93519f556d408b295b
Panagiotis_INS <39010461+Panagiotis-INS@users.noreply.github.com>
2021-01-17 22:13:53 +0200

    Update pass_gen.sh

--- Random_password_generator/pass_gen.sh
+++ Random_password_generator/pass_gen.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 ##
 pass="";
+if(($#==0))
+then
+	len=10;
+else
+	len=$1;
+fi;
 seed=`date |cut -d " " -f 5`;
 echo $seed >.config;
-pass=`md5sum .config|cut -d " " -f 1 |base64 |head -c 10`;
+pass=`md5sum .config|cut -d " " -f 1 |base64 |head -c $len`;
 echo $pass;
 exit 0;
