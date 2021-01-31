8a4dfb94e7d3cc0f16624013bacb57bd4cde887b
Panagiotis_INS <39010461+Panagiotis-INS@users.noreply.github.com>
2021-01-31 23:49:42 +0200

    Update Rick.sh

--- Rick Script/Rick.sh
+++ Rick Script/Rick.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 ##
+#code version
 enc=cGFkZGluZ3BhZGRpbmdwYWRkaW5nIGh0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9ZFF3NHc5V2dYY1EgcGFkZGlucGFkZGluZ3BhZGRpbmcK
 xdg-open `echo $enc |base64 -d |cut -d " " -f 2`;
 exit 0;
 #
+#command version
 xdg-open `echo "cGFkZGluZ3BhZGRpbmdwYWRkaW5nIGh0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9ZFF3NHc5V2dYY1EgcGFkZGlucGFkZGluZ3BhZGRpbmcK" |base64 -d | cut -d " " -f 2`
