fd110bd728d52e4e7867f4c056143e9318701729
Panagiotis_INS <39010461+Panagiotis-INS@users.noreply.github.com>
2021-01-31 23:49:03 +0200

    Add files via upload

---
+++ Rick Script/Rick.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+##
+enc=cGFkZGluZ3BhZGRpbmdwYWRkaW5nIGh0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9ZFF3NHc5V2dYY1EgcGFkZGlucGFkZGluZ3BhZGRpbmcK
+xdg-open `echo $enc |base64 -d |cut -d " " -f 2`;
+exit 0;
+#
+xdg-open `echo "cGFkZGluZ3BhZGRpbmdwYWRkaW5nIGh0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9ZFF3NHc5V2dYY1EgcGFkZGlucGFkZGluZ3BhZGRpbmcK" |base64 -d | cut -d " " -f 2`
